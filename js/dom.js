ade222ce70c84cf1898f0fbd67c22db75f388541
galibcse19 <mrgalib40@gmail.com>
2024-01-31 23:30:22 +0600

    uploded some event work

---
+++ js/dom.js
@@ -0,0 +1,8 @@
+console.log("galib");
+console.log(document.body);
+
+const value =document.getElementById("value")
+// console.log(value.innerText);
+
+const v1= document.querySelector('#value')
+console.log(v1.InnerText);
